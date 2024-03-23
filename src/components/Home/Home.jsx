9481da0e1fe0d032ebb2b178ca506f04e6d348e6
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-03-23 12:06:56 +0100

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Meta from './Meta/Meta';
+import MovieList from '../MovieList/MovieList';
 
 const Home = () => {
   return (
@@ -7,6 +8,7 @@ const Home = () => {
       <div>
         <Meta title="Sopravvissuto" subTitle="The Martian" desc="Un astronauta dato per morto dopo una tempesta di sabbia viene abbandonato su Marte e lotta per sopravvivere su un pianeta ostile, inviando un messaggio a casa." />
       </div>
+      <MovieList />
     </div>
     
   );
--- src/components/Home/Meta/Meta.jsx
+++ src/components/Home/Meta/Meta.jsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import './meta.css';
+import { Button } from'react-bootstrap';
+import { FaPlay } from 'react-icons/fa';
+import { FaCircleInfo  } from "react-icons/fa6";
 
 const Meta = (props) => {
     const { title, subTitle, desc } = props;
@@ -9,6 +12,20 @@ const Meta = (props) => {
         <h2 className='metaContainer-subtitle text-uppercase'>{subTitle}</h2>
         <div className=''>
             <p className='metaContainer-desc'>{desc}</p>
+        </div>
+        <div className='d-flex'>
+          <Button variant='light'>
+            <div className='d-flex justify-content-center align-items-center py-2 px-3 mx-2'>
+              <FaPlay className='fa-play' />
+              <span className='ms-3 riproduci'>Riproduci</span>
+            </div>
+          </Button>
+          <Button variant='secondary' className='mx-3 button-info'>
+            <div className='d-flex justify-content-center align-items-center py-2 px-3'>
+              <FaCircleInfo  className='FaCircleInfo ' />
+              <span className='ms-3 altre-info'>Altre info</span>
+            </div>
+          </Button>
         </div>
         
     </div>
