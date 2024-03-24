f36d6acc3e030e29cd62e178d3d5989e210eb870
CodeKaito <jerome.decinco@maxwell.mi.it>
2024-03-24 09:07:07 +0100

    update

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import Meta from './Meta/Meta';
-import MovieList from '../MovieList/MovieList';
+import NetflixList from '../NetflixList/NetflixList';
 import './home.css';
 
 const Home = () => {
@@ -9,7 +9,7 @@ const Home = () => {
       <div>
         <Meta title="Sopravvissuto" subTitle="The Martian" desc="Un astronauta dato per morto dopo una tempesta di sabbia viene abbandonato su Marte e lotta per sopravvivere su un pianeta ostile, inviando un messaggio a casa." />
       </div>
-      <MovieList />
+      <NetflixList searchQuery="harry potter" />
     </div>
     
   );
---
+++ src/components/NetflixList/NetflixList.jsx
@@ -0,0 +1,94 @@
+import React, { useState, useEffect } from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import './netflixlist.css'
+
+import { Link } from "react-router-dom";
+import NetflixCard from "../NetflixCard/NetflixCard";
+
+const NetflixList = ({ searchQuery }) => {
+  // Utilizzo dello stato con useState per i dati e il caricamento
+  const [data, setData] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  // Impostazioni per lo slider
+  const settings = {
+    dots: false,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 6,
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 4,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 1,
+        },
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+        },
+      },
+    ],
+  };
+
+  // Dopo aver determinato useState, effettuo la fetch con useEffect
+  useEffect(() => {
+    const fetchData = async () => {
+      const token = "d0b72ce5";
+      const endpoint = `http://www.omdbapi.com/?apikey=${token}&movie&s=${searchQuery}`;
+
+      try {
+        const response = await fetch(endpoint);
+        if (response.ok) {
+          const responseData = await response.json();
+          // Aggiorno lo stato con i dati ottenuti dalla chiamata API
+          setData(responseData);
+          setIsLoaded(true);
+          console.log("Successo durante il recupero dei dati");
+        } else {
+          console.log("Errore durante il recupero dei dati");
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    // Chiamata alla funzione di fetch
+    fetchData();
+  }, [searchQuery]); // Dipendenza da props.searchQuery
+
+  // Rendering di tutto (con return, NON render)
+  return (
+    <div className="mt-5">
+      {/* Utilizzo dello Slider con le impostazioni specificate */}
+      <Slider {...settings}>
+        {/* Mappo i dati ottenuti dalla chiamata API */}
+        {data.Search &&
+          data.Search.map((movie) => (
+            <div key={movie.imdbID}>
+              {/* Link per rendere cliccabile la locandina */}
+              <Link to={`/movie-details/${movie.imdbID}`}>
+                {/* Componente SingleMovie con l'immagine del film */}
+                <NetflixCard img={movie.Poster} />
+              </Link>
+            </div>
+          ))}
+      </Slider>
+    </div>
+  );
+};
+
+export default NetflixList;
